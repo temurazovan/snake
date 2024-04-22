99a2e0c10b889ca6ca7668e93e4ac2b7c78f3037
Nadezhda <nadezhdadieslast@gmail.com>
2024-04-23 00:40:11 +0400

    now it's a snake!

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 
 void initialization(int arr[5][5]) {
-    int count = -1;
+    int count = 0;
+    int multiplier = 0;
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            count++;
             arr[i][j] = count;
+            count++;
         }
+        multiplier *= -1;
     }
 }
 
 void printArray(int arr[5][5]) {
+    int multiplier = 1;
+    int rowNum = 0;
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             std::cout << arr[i][j] << " ";
+            rowNum += multiplier;
         }
+        multiplier *= -1;
+        rowNum += multiplier;
         std::cout << std::endl;
     }
 }
